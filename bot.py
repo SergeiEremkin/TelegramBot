a079817e544e97badb9ca5fae9d8012e7e03cdb4
Sergei Eremkin <ffr@list.ru>
2022-08-30 23:41:38 +0300

    added easy tg bot

--- bot.py
+++ bot.py
@@ -3,31 +3,37 @@ import json
 from random import *
 from json import *
 
+films = []
+
 try:
     load()
 except:
-    films =[]
+    
 films.append("Матрица")
 films.append("Солярис")
 films.append("Властелин колец")
 films.append("Техасская резня бензопилой")
 films.append("Санта Барбара")
 
+
 def save():
-    with open("films.json","w", encoding="utf-8") as fh:
-        fh.write(json.dumps(films, ensure_ascii = False))
+    with open("films.json", "w", encoding="utf-8") as fh:
+        fh.write(json.dumps(films, ensure_ascii=False))
         print("Наша фильмотека была успешно сохранена в файле films.json")
 
+
 def load():
-    with open("films.json","r", encoding="utf-8") as fh:
-        films=json.load(fh)
+    global films
+    with open("films.json", "r", encoding="utf-8") as fh:
+        films = json.load(fh)
         print("Фильмотека была успешно загружена")
 
+
 while True:
-    command=input("Введите команду ")
+    command = input("Введите команду ")
     if command == "/start":
         print("Бот-фильмотека начал свою работу")
-    elif command =="/stop":
+    elif command == "/stop":
         save()
         print("Бот остановил свою работу. Заходите еще, будем рады")
         break
@@ -38,22 +44,22 @@ while True:
         f = input("Введите название фильмы")
         films.append(f)
         print("Фильм был успешно добавлен в коллекцию")
-    elif command =="/help":
+    elif command == "/help":
         print("здесь какой-то мануал")
     elif command == "/delete":
         f = input("Введите название фильмы")
         try:
-           films.remove(f)
-           print("Фильм был удален успешно")
+            films.remove(f)
+            print("Фильм был удален успешно")
         except:
-            print("Такого фильмы нет в фильмотеке") 
+            print("Такого фильмы нет в фильмотеке")
     elif command == "/random":
         # rnd = randint(0, len(films)-1)
-        # print("Слепой жребий показал вам -" + films[rnd])   
+        # print("Слепой жребий показал вам -" + films[rnd])
         print("Слепой жребий показал вам -" + choice(films))
-    elif command =="/save":
+    elif command == "/save":
         save()
-    elif command =="/load":
+    elif command == "/load":
         load()
     else:
         print("Неопознанная комнада. Просьба изучить мануал /help")
